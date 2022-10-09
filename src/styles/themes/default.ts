1685fc4e8dec0b7a7b43f79bfa72e5a4baddedbb
Lucian AF <lucian_af@hotmail.com>
2022-10-09 11:15:24 -0300

    chore: criação do projeto e primeiras configurações;

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  "blue-100": "#E7EDF4",
+  "blue-200": "#C4D4E3",
+  "blue-300": "#3294F8",
+  "blue-400": "#AFC2D4",
+  "blue-500": "#7B96B2",
+  "blue-600": "#3A536B",
+
+  "squid-500": "#1C2F41",
+  "squid-600": "#112131",
+  "squid-700": "#0B1B2B",
+
+  "gray-800": "#071422",
+  "gray-900": "#040F1A",
+} as const;
